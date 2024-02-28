600c226d75f253c49683c4841f53edbb5b334890
Abraham Morales Arroyo <abrahamof606@gmail.com>
2024-02-28 12:58:38 -0600

    Initial Commit

---
+++ UserLocationOnMap/UserLocationOnMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserLocationOnMapApp.swift
+//  UserLocationOnMap
+//
+//  Created by Abraham Morales Arroyo on 2/28/24.
+//
+
+import SwiftUI
+
+@main
+struct UserLocationOnMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
