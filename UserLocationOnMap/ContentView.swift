cb74713825fd7e4e7f80cbc22e3f242d1216d968
Abraham Morales Arroyo <abrahamof606@gmail.com>
2024-02-29 07:12:08 -0600

    The code allows the user to show up in the apple maps. They will show up as a blue dot.

--- UserLocationOnMap/ContentView.swift
+++ UserLocationOnMap/ContentView.swift
@@ -6,19 +6,28 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    
+    @State private var position: MapCameraPosition = .userLocation(fallback: .automatic)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Map(position: $position) {
+            
+            
         }
-        .padding()
+        .mapControls {
+            MapUserLocationButton()
+            MapPitchToggle()
+        }
+        .onAppear(){
+            CLLocationManager().requestWhenInUseAuthorization()
+        }
     }
 }
 
 #Preview {
     ContentView()
 }
+//to change the locaiton of the map you are going to need the simulator running and once in the simulator got to the features tab and click on location.
